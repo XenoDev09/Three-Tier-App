0b39c7bbd3a549770e8d623f3331d5221f75b650
Nischal Moktan <nischal123321@gmail.com>
2026-03-11 21:56:23 +0545

    Tried for CICD pipeline for frontend

--- frontend/src/components/UserForm.js
+++ frontend/src/components/UserForm.js
@@ -30,7 +30,7 @@ const UserForm = ({ onUserCreated }) => {
 
   return (
     <div>
-      <h2>Add New User</h2>
+      <h2>Add New User For Testing : </h2>
       <form onSubmit={handleSubmit} data-testid="user-form">
         <div className="form-group">
           <label htmlFor="name">Name:</label>
